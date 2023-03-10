8671120296ffba1b2d8d17c4a787c2d92a995176
Ann-Kristin Seifer <ann-kristin.seifer@fau.de>
2023-03-10 10:32:16 +0100

    bug fix: in check installation example

--- examples/check_installation/check_installation.py
+++ examples/check_installation/check_installation.py
@@ -12,9 +12,9 @@ Check installation
 
 from eargait.event_detection import DiaoAdaptedEventDetection
 from eargait.preprocessing import aling_gravity_and_convert_ear_to_ebf, convert_ear_to_ebf, load
-from eargait.utils.example_data import get_example_data_path
+from eargait.utils.example_data import get_mat_example_data_path
 
 # data directory
-data_path = get_example_data_path()
+data_path = get_mat_example_data_path()
 
 print("\nInstallation was successful.\n")
