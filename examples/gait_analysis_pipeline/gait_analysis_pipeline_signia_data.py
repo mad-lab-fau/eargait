0800b320e25a09ed2bb2214590c6a70768fd3e67
Ann-Kristin Seifer <ann-kristin.seifer@fau.de>
2023-05-03 14:25:34 +0200

    bug fixes von kaja

--- examples/gait_analysis_pipeline/gait_analysis_pipeline_signia_data.py
+++ examples/gait_analysis_pipeline/gait_analysis_pipeline_signia_data.py
@@ -22,7 +22,7 @@ import pandas as pd
 
 from eargait import EarGait
 from eargait.event_detection import DiaoAdaptedEventDetection
-from eargait.preprocessing import aling_gravity_and_convert_ear_to_ebf, load
+from eargait.preprocessing import align_gravity_and_convert_ear_to_ebf, load
 from eargait.spatial_params import SpatialParamsExample
 from eargait.utils.example_data import get_mat_example_data_path
 
@@ -34,8 +34,8 @@ data_path = get_mat_example_data_path()
 # ----------------
 #
 # A data session refers to a recording by signia hearing aids.
-# A session can consist of a single '*.txt' or `*.mat` file, or two `*.mat` files, for left and right ear, respectively.
-# The session is loaded using the local path data_path` of the directory, in which the matlab/txt file(s) are stored.
+# A session can consist of a single `*.txt` or `*.mat` file, or two `*.mat` files, for left and right ear, respectively.
+# The session is loaded using the local path `data_path` of the directory, in which the matlab/txt file(s) are stored.
 # For more options regarding loading the data see :ref:`example_load_data`.
 target_sample_rate = 50
 session = load(data_path, target_sample_rate_hz=target_sample_rate, skip_calibration=True)
@@ -47,7 +47,7 @@ session.info
 #
 # Align session to gravity and transform coordinate system into body frame
 
-ear_data = aling_gravity_and_convert_ear_to_ebf(session)
+ear_data = align_gravity_and_convert_ear_to_ebf(session)
 
 # Alternatively, you can skip the gravity alignment by using the following function:  convert_ear_to_ebf
 # ear_data = convert_ear_to_ebf(session)
@@ -57,7 +57,7 @@ ear_data = aling_gravity_and_convert_ear_to_ebf(session)
 # ------------------------
 #
 # Note: Here prior knowledge about walking sequence within the given data session are applied.
-# load csv file containing walking bouts
+# Load csv file containing walking bouts.
 rescale_factor = 200 / target_sample_rate
 walking_bout_list = pd.read_csv(data_path.parent.joinpath("walking_bout_indices.csv"))
 interval = (int(walking_bout_list["start"][0] / rescale_factor), int(walking_bout_list["stop"][0] / rescale_factor))
@@ -74,9 +74,9 @@ for side in ear_data.keys():
 # --------------------------------------
 #
 # Recommended parameters:
-# apply filter = True <br />
-# sampling_rate_hz needs to correspond to target_sample_rate_hz <br />
-# window_length should be equal to sampling_rate_hz
+# `apply filter = True`,
+# `sampling_rate_hz` needs to correspond to target_sample_rate_hz,
+# * window_length` should be equal to sampling_rate_hz.
 
 event_detection_algorithm = DiaoAdaptedEventDetection(
     sample_rate_hz=target_sample_rate, window_length=target_sample_rate
@@ -95,7 +95,7 @@ spatial_method = SpatialParamsExample(target_sample_rate)
 # -----------------------------------
 #
 # Recommended parameters:
-# sampling_rate_hz needs to correspond to target_sample_rate_hz
+# `sampling_rate_hz`needs to correspond to `target_sample_rate_hz`.
 ear_gait = EarGait(
     sample_rate_hz=target_sample_rate,
     event_detection_method=event_detection_algorithm,
