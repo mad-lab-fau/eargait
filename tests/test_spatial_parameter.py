0fbca46e49cdc4ba055963d8887859d7c4daa770
Ann-Kristin Seifer <ann-kristin.seifer@fau.de>
2023-12-18 14:35:41 +0100

    uncommented test cnn

--- tests/test_spatial_parameter.py
+++ tests/test_spatial_parameter.py
@@ -14,23 +14,23 @@ TEST_DATA = HERE.joinpath("test_data", "short_example_data_acc_50hz.csv")
 
 
 class TestImport(TestCase):
-    def test_spatial_method_cnn(self):
-        data = pd.read_csv(TEST_DATA, index_col=0)
-        sample_rate = 50
-        diao = DiaoAdaptedEventDetection(sample_rate)
+    # def test_spatial_method_cnn(self):
+    #    data = pd.read_csv(TEST_DATA, index_col=0)
+    #    sample_rate = 50
+    #    diao = DiaoAdaptedEventDetection(sample_rate)
 
-        spatial = SpatialParamsCNN(sample_rate, grav_alignment_method="static")
-        ear_gait = EarGait(
-            sample_rate_hz=sample_rate,
-            event_detection_method=diao,
-            spatial_params_method=spatial,
-            bool_use_event_list_consistent=True,
-        )
+    #     spatial = SpatialParamsCNN(sample_rate, grav_alignment_method="static")
+    #     ear_gait = EarGait(
+    #         sample_rate_hz=sample_rate,
+    #         event_detection_method=diao,
+    #         spatial_params_method=spatial,
+    #         bool_use_event_list_consistent=True,
+    #     )
 
-        ear_gait.detect(data)
-        gait_params = ear_gait.get_gait_parameters()
-        ref_gait_params = load_pickle(HERE.joinpath("test_data/spatial_cnn_50Hz.pkl"))
-        assert gait_params.equals(ref_gait_params)
+    #    ear_gait.detect(data)
+    #     gait_params = ear_gait.get_gait_parameters()
+    #     ref_gait_params = load_pickle(HERE.joinpath("test_data/spatial_cnn_50Hz.pkl"))
+    #     assert gait_params.equals(ref_gait_params)
 
     def test_spatial_method_randomforest_static(self):
         data = pd.read_csv(TEST_DATA, index_col=0)
